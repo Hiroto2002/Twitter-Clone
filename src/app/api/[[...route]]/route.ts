6757e48c565df6d990bfda7b0a3b46c516548a04
Hiroto2002 <hiroto2002h14@icloud.com>
2024-10-24 01:33:23 +0900

    feat:test,entity,valueobject

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -5,12 +5,9 @@ import posts from "./posts";
 
 // export const runtime = "edge";
 
-// basePath は API ルートのベースパスを指定します
-// 以降、新たに生やす API ルートはこのパスを基準に追加されます
 const app = new Hono().basePath("/api");
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const route = app.route("/posts", posts);
-
 export type AppType = typeof route;
 
 export const GET = handle(app);
