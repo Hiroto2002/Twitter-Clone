09eb458b2f92a2462323e8e6288d091de4e6e415
Hiroto2002 <hiroto2002h14@icloud.com>
2024-10-23 22:36:34 +0900

    fix:build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // ...
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
+      })
+    );
+
+    return config;
+  },
+};
 
 export default nextConfig;
